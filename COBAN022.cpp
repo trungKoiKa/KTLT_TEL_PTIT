2eb59ae355ae2c4d0385180f1bdc55ba5a4e56da
hoanganh04-11 <ht.anh00411@gmail.com>
2026-07-09 11:48:42 +0700

    COBAN002 Mua Sô-cô-la

---
+++ COBAN022.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		int x, y, m; cin >> x >> y >> m;
+		
+		int res = m/x;
+		int wrap = res;
+		
+		while(wrap >= y){
+			int doi = wrap / y;
+			res += doi;
+			
+			wrap = doi + wrap % y;
+		}
+		cout << res << endl;
+	}
+}
