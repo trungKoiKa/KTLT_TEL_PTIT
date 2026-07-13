70013a773fa3bd718137a86d6d588aadbe9e0800
hoanganh04-11 <ht.anh00411@gmail.com>
2026-07-13 10:44:49 +0700

    KTDAYSO001 Dãy cấp số cộng

--- KTDAYSO001.cpp
+++ KTDAYSO001.cpp
@@ -1,31 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
+ll a[1000001];
+
+int check(ll a[], int n){
+	ll d = a[1] - a[0];
+	
+	for(int i = 2; i < n; i++){
+		if(a[i] - a[i-1] != d) return 0;
+	}
+	return 1;
+}
 
 
 int main(){
 		int t; cin >> t;
 		while(t--){
 			int n; cin >> n;
-			int a[n];
-			for(int i = 0; i < n; i++){
-				cin >> a[i];
-			}	
-			if(n <= 2){
-				cout << "YES" << endl;
-				continue;
-			}
-			int d = a[1] - a[0];
-			bool ok = true;
+			for(int i=0; i<n; i++) cin >> a[i];
 			
-			for(int i = 2; i < n; i++){
-				if(a[i] - a[i-1] != d){
-					cout << "NO" << endl;
-					ok = false;
-					break;
-				}
-			}
-			if(ok) cout << "YES" << endl;
+			if(check(a, n)) cout << "YES\n";
+			else cout << "NO\n";
 		}
 		
 }
