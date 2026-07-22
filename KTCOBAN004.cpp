3bffa578fe085313654553ed54711198c0ce91e0
hoanganh04-11 <ht.anh00411@gmail.com>
2026-07-22 09:42:58 +0700

    KTCOBAN004 Số đối xứng

---
+++ KTCOBAN004.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long 
+
+int main(){
+	int t; cin >> t;
+	
+	while(t--){
+		int k; cin >> k;
+        ll ans = 9 * pow(10, (k-1)/2);
+		cout << ans << endl;
+	}
+}
